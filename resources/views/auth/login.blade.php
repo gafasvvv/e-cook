129cacbf074975e44d2f4fac4f326ece247373f8
gafas <ry0suke.programming@gmail.com>
2018-12-25 08:31:26 +0000

    user login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Log in</h1>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            
+            {!! Form::open(['route' => 'login.post']) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+            
+            <p class="mt-2">ユーザ登録がまだの方は→{!! link_to_route('signup.get', 'ユーザ登録') !!}</p>
+        </div>
+    </div>
+@endsection
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,8 +9,19 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
+                @if(Auth::check())
+                <li class="nav-item"><a href="#" class="nav-link">ユーザ</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-item"><a href="#">マイページ</a></li>
+                        <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                    </ul>
+                </li>
+                @else
                 <li>{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                @endif
             </ul>
         </div>
     </nav>
