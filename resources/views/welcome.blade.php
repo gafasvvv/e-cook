9639a3d8c9f21017ebd3d86ab8a84852be73b946
gafas <ry0suke.programming@gmail.com>
2018-12-25 07:54:18 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,5 @@
-@expands('layouts.app')
+@extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>e-cook</h1>
-        </div>
-    </div>
+    
 @endsection
