520911067291c8efbb30243bebe305bf2c06af7f
gafas <ry0suke.programming@gmail.com>
2018-12-25 09:54:28 +0000

    user page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,17 +10,16 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 @if(Auth::check())
-                <li class="nav-item"><a href="#" class="nav-link">ユーザ</a></li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                     <ul class="dropdown-menu dropdown-menu-right">
-                        <li class="dropdown-item"><a href="#">マイページ</a></li>
+                        <li class="dropdown-item">{!! link_to_route('users.show', 'ユーザーページ', ['id' => Auth::id()]) !!}</li>
                         <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
                     </ul>
                 </li>
                 @else
-                <li>{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                <li class="nav-item">{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
                 @endif
             </ul>
         </div>
